f0da13f94d87d1ac078b3a1fa523231e5793b9c3
sv222 <sv_golang@mail.ru>
2022-01-23 19:57:22 +0100

    add page handler

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -66,3 +66,22 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("could not execute template: %v", err)
 	}
 }
+
+func PageHandler(w http.ResponseWriter, r *http.Request) {
+	tmpls := []string{
+		"./web/base.layout.gohtml",
+		"./web/footer.layout.gohtml",
+		"./web/header.layout.gohtml",
+	}
+
+	tmpl, err := template.ParseFiles(tmpls...)
+	if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		log.Fatalf("could not parse template: %v", err)
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		log.Fatalf("could not execute template: %v", err)
+	}
+}
