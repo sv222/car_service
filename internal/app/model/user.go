aa7c630cc4f0c5cfe07514d8da1463baf91ed8c4
sv222 <sv_golang@mail.ru>
2022-01-26 14:13:08 +0100

    add user model

---
+++ internal/app/model/user.go
@@ -0,0 +1,8 @@
+package model
+
+type User struct {
+	ID                int64  `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
+}
